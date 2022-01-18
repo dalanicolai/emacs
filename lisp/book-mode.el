50b0a60f8bd743fe9e2f08ec0e7b2b7e161e779b
Daniel Nicolai <dalanicolai@gmail.com>
2022-01-18 14:49:22 +0100

    Test: add placeholders after invisible content/control overlay

--- lisp/book-mode.el
+++ lisp/book-mode.el
@@ -1,5 +1,16 @@
 (require 'image-mode)
+(require 'svg)
 
+;; TODO remove helper snippets
+(dotimes (i 3)
+  (set (intern (format "im%s" (1+ i))) (let* ((w 800)
+                                              (h 1600)
+                                              (svg (svg-create w h)))
+                                         (svg-rectangle svg 0 0 w h :fill-color (pcase i
+                                                                                  (0 "red")
+                                                                                  (1 "green")
+                                                                                  (2 "blue")))
+                                         (svg-image svg))))
 
 (defvaralias 'book-mode-winprops-alist 'image-mode-winprops-alist)
 
@@ -13,5 +24,20 @@
 (defalias 'book-mode-reapply-winprops 'image-mode-reapply-winprops)
 (defalias 'book-mode-setup-winprops 'image-mode-setup-winprops)
 
+;;;###autoload
 (define-derived-mode book-mode special-mode "Book"
-  (book-mode-setup-winprops))
+  ;; (add-hook 'book-mode-new-window-functions
+	;;           #'doc-view-new-window-function nil t)
+  (book-mode-setup-winprops)
+  (let ((contents-end (point-max))
+        (inhibit-read-only t))
+    (goto-char (point-max))
+    ;; (insert "\n")
+    (insert "1")
+    (let ((ol (make-overlay (point-min) contents-end))
+          (ol2 (make-overlay (1- (point-max)) (point-max))))
+      (insert "\n2")
+      (let ((ol3 (make-overlay (1- (point-max)) (point-max))))
+        (overlay-put ol 'invisible t)
+        (overlay-put ol2 'display im1)
+        (overlay-put ol3 'display im2)))))
