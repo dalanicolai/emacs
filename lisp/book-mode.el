fb6af5b9cb60b01bb5c16be6038feec897a9377e
Daniel Nicolai <dalanicolai@gmail.com>
2022-01-18 18:46:08 +0100

    Remove book- to image-mode aliases
    The aliases are not essential and if desired, can be recreated at any
    time

--- lisp/book-mode.el
+++ lisp/book-mode.el
@@ -12,23 +12,24 @@
                                                                                   (2 "blue")))
                                          (svg-image svg))))
 
-(defvaralias 'book-mode-winprops-alist 'image-mode-winprops-alist)
+;; TODO maybe add back aliases
+;; (defvaralias 'book-mode-winprops-alist 'image-mode-winprops-alist)
 
-(defvaralias 'book-mode-new-window-functions 'image-mode-new-window-functions)
+;; (defvaralias 'book-mode-new-window-functions 'image-mode-new-window-functions)
 
-(defalias 'book-mode-winprops 'image-mode-winprops)
-(defalias 'book-mode-window-get 'image-mode-window-get)
-(defalias 'book-mode-window-put 'image-mode-window-put)
-(defalias 'book-set-window-vscroll 'image-set-window-vscroll)
-(defalias 'book-set-window-hscroll 'image-set-window-hscroll)
-(defalias 'book-mode-reapply-winprops 'image-mode-reapply-winprops)
-(defalias 'book-mode-setup-winprops 'image-mode-setup-winprops)
+;; (defalias 'book-mode-winprops 'image-mode-winprops)
+;; (defalias 'book-mode-window-get 'image-mode-window-get)
+;; (defalias 'book-mode-window-put 'image-mode-window-put)
+;; (defalias 'book-set-window-vscroll 'image-set-window-vscroll)
+;; (defalias 'book-set-window-hscroll 'image-set-window-hscroll)
+;; (defalias 'book-mode-reapply-winprops 'image-mode-reapply-winprops)
+;; (defalias 'book-mode-setup-winprops 'image-mode-setup-winprops)
 
 ;;;###autoload
 (define-derived-mode book-mode special-mode "Book"
   ;; (add-hook 'book-mode-new-window-functions
 	;;           #'doc-view-new-window-function nil t)
-  (book-mode-setup-winprops)
+  (image-mode-setup-winprops)
   (let ((contents-end (point-max))
         (inhibit-read-only t))
     (goto-char (point-max))
